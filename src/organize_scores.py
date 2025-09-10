b8e56028c9274493a1524630523a95201f65f696
gkiar <gkiar@computer>
2025-09-10 19:56:47 -0400

    added script for merging and organizing test set performance data

---
+++ src/organize_scores.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import os.path as op
+import pandas as pd
+import numpy as np
+
+
+# List of relevant files
+bp = '/data/gkiar/kaggle'
+f_submission_files = op.join(bp, 'files.txt')  # Bash-collected list of all local parquet filepaths for each competitor
+f_solution = op.join(bp, 'solution.csv')  # True solution for each sequence
+f_test_sequences = op.join(bp, 'test.csv')  # Test CSV file that competitors used
+f_aggregated = op.join(bp, 'top20results.parquet')  # File where we will store results
+
+# Load the list of submission records
+with open(f_submission_files, 'r') as fhandle:
+    submissions = fhandle.readlines()
+
+# For every submission, load the file and add it to a list.
+df_subs = [pd.read_parquet(sub.strip())  # Note: they are labeled as CSV files for some reason, but are parquet...
+           for sub in submissions] 
+
+# Seed the table we'll merge the results into
+df_agg = df_subs[0]
+for idx, tdf in enumerate(df_subs[1:]):
+    df_agg = df_agg.merge(tdf, on="sequence_id", suffixes=["", f"{idx+1}"])  # Merge on sequence ID, add index to predictions
+
+# Relabel the first index to make it similar to the rest
+df_agg.rename(columns={"gesture": "gesture0"}, inplace=True)
+
+# Load the solutions table, and add it into the aggregate table
+df_sol = pd.read_csv(f_solution)
+df_agg = df_agg.merge(df_sol, on="sequence_id")
+
+# Relabel the gesture column to clearly indicate true value
+df_agg.rename(columns={"gesture": "gesture_true"}, inplace=True)
+
+# Load the complete series of test data
+df_test = pd.read_csv(f_test_sequences)
+
+# For every sequence ID, evaluate if the thermopile data (thm_1) were real or NaNs
+seq_all_sensors = []
+for seq in df_test['sequence_id'].unique():
+    tdf = df_test.query(f'sequence_id == "{seq}"')
+    tthm_mn = tdf['thm_1'].mean()
+    seq_all_sensors += [{"sequence_id": seq,
+                         "all_sensors": bool(tthm_mn < np.inf)}]
+
+# Add data completeness 
+df_agg = df_agg.merge(pd.DataFrame.from_dict(seq_all_sensors), on="sequence_id")
+
+# Save to file
+df_agg.to_parquet(f_aggregated)
